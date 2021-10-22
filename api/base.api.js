7b35b85b55d835bad9a7483b8d95c659d019d628
victorratts13 <victor.ratts13@gmail.com>
2021-10-22 13:19:17 -0300

    new process

--- api/base.api.js
+++ api/base.api.js
@@ -10,7 +10,7 @@ var header = {
      'cache-control': ' max-age=0',
      //'sec-ch-ua': '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
      //'sec-ch-ua-platform': '"Linux"',
-     'cookie': '_gcl_au=1.1.1982248783.1631309980; 888Cookie=lang%3Dpt%26OSR%3D485472%26RefType%3DSearchEngine%26TestData%3D%7B%22country%22%3A%22bra%22%2C%22last-referrer%22%3A%22https%3A%2F%2Fwww.google.com%2F%22%2C%22orig-lp%22%3A%22https%3A%2F%2Fwww.888sport.com%2Fpt%2F%22%2C%22referrer%22%3A%22https%3A%2F%2Fwww.google.com%22%7D; _gid=GA1.2.550140437.1634869424; bbsess=O7cK4iDToZUSq%2CRnt193ywBqBN3; lang=por; odds_format=DECIMAL; spectate_session=859e30dc-703b-4d0b-8b70-cb3201a36629%3Aanon; _ga=GA1.2.126253252.1631309980; _gat_UA-125725186-7=1; _ga_T9PNVVL13N=GS1.1.1634869424.2.1.1634870608.60',
+     'cookie': process.env.COOKIE || '_gcl_au=1.1.1982248783.1631309980; 888Cookie=lang%3Dpt%26OSR%3D485472%26RefType%3DSearchEngine%26TestData%3D%7B%22country%22%3A%22bra%22%2C%22last-referrer%22%3A%22https%3A%2F%2Fwww.google.com%2F%22%2C%22orig-lp%22%3A%22https%3A%2F%2Fwww.888sport.com%2Fpt%2F%22%2C%22referrer%22%3A%22https%3A%2F%2Fwww.google.com%22%7D; _gid=GA1.2.550140437.1634869424; bbsess=O7cK4iDToZUSq%2CRnt193ywBqBN3; lang=por; odds_format=DECIMAL; spectate_session=859e30dc-703b-4d0b-8b70-cb3201a36629%3Aanon; _ga=GA1.2.126253252.1631309980; _gat_UA-125725186-7=1; _ga_T9PNVVL13N=GS1.1.1634869424.2.1.1634870608.60',
      //newrelic: 'eyJ2IjpbMCwxXSwiZCI6eyJ0eSI6IkJyb3dzZXIiLCJhYyI6IjI1MDk2NzQiLCJhcCI6IjMwNzYyODM3IiwiaWQiOiIzYTg3ODBjMmU1MzY1Y2U3IiwidHIiOiI2MWI5MTJmOWNkYmU0MjI5IiwidGkiOjE2MzQ4NzA2MDkzMjd9fQ==',
      origin: 'https://www.888sport.com',
      referer: 'https://www.888sport.com/',
